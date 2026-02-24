b81fc8e0093f3c4a489099e2b2d730a87861a2ed
Anders Oedlund <aodlund@attaindata.io>
2026-02-24 15:26:53 -0800

    Fix pigeon generation

--- packages/core/build_pigeon_plugins.sh
+++ packages/core/build_pigeon_plugins.sh
@@ -4,6 +4,7 @@ mkdir -p android/main/kotlin
 flutter pub run pigeon \
   --input pigeon/context.dart \
   --dart_out lib/native_context.dart \
-  --experimental_swift_out ios/Classes/Context.swift \
-  --experimental_kotlin_out android/src/main/kotlin/com/hightouch/events/Context.kt \
-  --java_package "com.hightouch.flutter"
+  --swift_out ios/Classes/Context.swift \
+  --kotlin_out android/src/main/kotlin/com/hightouch/events/Context.kt \
+  --java_package "com.hightouch.events" \
+  --kotlin_package "com.hightouch.events"
